9a991303af5224ea925ff2cb889cb4a87dcc3441
joseduardoaA <jose.cfeteit@gmail.com>
2023-11-15 19:41:03 -0600

    Promesas(2)

--- 02-react-practica/src/bases/08-import-export.js
+++ 02-react-practica/src/bases/08-import-export.js
@@ -1,5 +1,5 @@
 import heroes, {owners} from "../data/heroes";
-console.log(owners)
+// console.log(owners)
 // Como importar un archivo de JavaScript
 
 // const getHeroeByID = (id) => {
@@ -13,12 +13,12 @@ console.log(owners)
 // console.log(getHeroeByID(2));
 
 
-const getHeroeByID = (id) => {
+export const getHeroeByID = (id) => {
     return heroes.find( (heroe) => heroe.id === id);
 }
-    console.log(getHeroeByID(2));
+    // console.log(getHeroeByID(2));
 
 // Esta funcion regresa los heroes que son de DC
 
-const getHeroesByOwner = (owner) => heroes.filter( (heroe) => heroe.owner === owner);
-console.log(getHeroesByOwner("DC"))
+export const getHeroesByOwner = (owner) => heroes.filter( (heroe) => heroe.owner === owner);
+// console.log(getHeroesByOwner("DC"))
--- 02-react-practica/src/index.js
+++ 02-react-practica/src/index.js
@@ -1,1 +1,15 @@
-import
+// PROMESAS
+import {getHeroeByID} from "./bases/08-import-export"
+
+const promesa = new Promise( (resolve, reject) => {
+    setTimeout( () => {
+        const heroe = getHeroeByID(2);
+        console.log(heroe)
+        // resolve();
+    }, 2000 );
+}); 
+
+promesa.then( ()=>{
+    console.log("Then de la promesa")
+} )
+
